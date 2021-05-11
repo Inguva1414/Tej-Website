3f80222f07b8dd14d280f3f6e5f5448a9c17211b
Inguva1414 <65541378+Inguva1414@users.noreply.github.com>
2021-05-11 23:46:06 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ function logMeIn(){
 			const email = formInputs[0].value;
 			sessionStorage.setItem("loginPx", email); 
 			const domain = formInputs[1].value;
-			const Cname = "Px" + domain;
+			const cname = "Px" + domain;
 			const name = formInputs[2].value.split(' ');
 			const firstName = name[0];
 			const lastName = name[1];
@@ -52,7 +52,7 @@ function logMeIn(){
 
 			},{
 				"id": domain, //Required		
-				"name":Cname
+				"name":cname
 			});
 			aptrinsic('set', 'globalContext', { "Project": "Tej-website"});
 //			console.log(email,firstName,lastName,userId,domain);
