3e84812a26875793b27ed3c57aa4ab7143bc6997
Inguva1414 <65541378+Inguva1414@users.noreply.github.com>
2021-08-05 17:48:55 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 //Run PX Tag
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+/*(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
 	(n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-ZYRA0KIWOQTB-2");
+aptrinsic("identify",
+			{
+				"id" : "singuva123compass",
+				
 
+			});*/
 
 
 (function (){
